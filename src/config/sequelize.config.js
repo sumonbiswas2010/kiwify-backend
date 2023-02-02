688ec78bb871513f71e84691c8cfb9585c1b54f9
Sumon Biswas <sumonbiswas2010@gmail.com>
2023-02-02 23:13:17 +0600

    vercel

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -5,7 +5,8 @@ const config = require('./config');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'mysql',
+  dialect:'mysql',
+  dialectModule: require('mysql2'),
   // logging: (...msg) => console.log(msg)
   logging: false,
 });
